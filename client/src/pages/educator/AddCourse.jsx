d3ca17472f5cd75fe35a646e8cab8122df4075db
akshat <kshatsharmaaa@gmail.com>
2025-05-08 17:40:36 +0530

    project completed

--- client/src/pages/educator/AddCourse.jsx
+++ client/src/pages/educator/AddCourse.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import uniqid from 'uniqid'
 import Quill from 'quill'
 import { assets } from '../../assets/assets'
+import { AppContext } from '../../context/AppContext'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 const AddCourse = () => {
+  const {backendUrl, getToken} = useContext(AppContext);
+
   const quillRef = useRef(null)
   const editorRef = useRef(null)
 
@@ -83,7 +88,41 @@ const AddCourse = () => {
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    try {
+      e.preventDefault()
+      if(!image) {
+        toast.error('Please upload course image')
+      }
+      const courseData = {
+        courseTitle,
+        coursePrice: Number(coursePrice),
+        discount: Number(discount),
+        courseDescription: quillRef.current.root.innerHTML,
+        courseContent: chapters,
+      }
+
+      const formData = new FormData();
+      formData.append('image', image);
+      formData.append('courseData', JSON.stringify(courseData));
+
+      const token = await getToken();
+      const {data} = await axios.post(backendUrl + '/api/educator/add-course', formData, {headers: {Authorization: `Bearer ${token}`}});
+
+      if(data.success) {
+        toast.success(data.message)
+        setCourseTitle('');
+        setCoursePrice(0);
+        setDiscount(0);
+        setChapters([]);
+        setImage(null);
+        quillRef.current.root.innerHTML = '';
+      }
+      else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
 
   useEffect(() => {
